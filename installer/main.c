5cf291927aef1411397de7c4f7538ff932012e38
iboshka24 <iboshka24@users.noreply.github.com>
2026-08-05 02:00:38 +0500

    fix: include gio/gio.h header and set -x in build workflow for C GTK4 installer

--- installer/main.c
+++ installer/main.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <gio/gio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -200,7 +201,7 @@ static void activate(GtkApplication *gtk_app, gpointer user_data) {
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("org.shobikaos.installer", 0);
+    GtkApplication *app = gtk_application_new("org.shobikaos.installer", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);

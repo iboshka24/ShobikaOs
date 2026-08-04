3c95f6d6baa746d20233a4f671979e30bb0815f4
iboshka24 <iboshka24@users.noreply.github.com>
2026-08-04 21:44:15 +0500

    fix: complete archiso profile rewrite - fix /dev/gpt-auto-root boot failure
    Root cause: missing mkinitcpio-archiso hooks, grub.cfg, syslinux configs,
    and locked root account prevented live ISO from booting.
    
    Changes:
    - Add mkinitcpio.conf.d/archiso.conf with full archiso hooks chain
    - Add grub/grub.cfg with proper archisobasedir/archisosearchuuid params
    - Add syslinux/ configs for BIOS boot (syslinux.cfg, archiso_sys.cfg, archiso_sys-linux.cfg)
    - Add /etc/shadow with unlocked root for live session
    - Add getty@tty1 autologin for live session
    - Fix profiledef.sh: add bootmodes, squashfs image type, file_permissions
    - Add critical packages: mkinitcpio-archiso, intel-ucode, amd-ucode, nbd, memtest86+-efi

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
+
 iso_name="shobikaos"
-iso_label="SHOBIKAOS_$(date +%Y%m)"
-iso_publisher="ShobikaOs <https://github.com/shobikaos>"
-iso_application="ShobikaOs"
-iso_version="$(date +%Y.%m.%d)"
+iso_label="SHOBIKAOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="ShobikaOs <https://github.com/iboshka24/ShobikaOs>"
+iso_application="ShobikaOs Live/Install DVD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootloader='grub_syslinux'
-airootfs_image_tool_options=('comp' 'xz' 'level' '1')
-arch=x86_64
-pacman_conf='pacman.conf'
+bootmodes=('bios.syslinux'
+           'uefi-x64.grub.esp'
+           'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/usr/bin/shobika-live-setup"]="0:0:755"
+)

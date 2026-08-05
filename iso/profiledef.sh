e8d7447acec461cbbd702fe1f94d4f2ab3b94d00
iboshka24 <iboshka24@users.noreply.github.com>
2026-08-05 22:56:57 +0500

    fix: switch UEFI bootmode to official systemd-boot for native mkarchiso compatibility

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -10,8 +10,8 @@ install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr'
            'bios.syslinux.eltorito'
-           'uefi-x64.grub.esp'
-           'uefi-x64.grub.eltorito')
+           'uefi-x64.systemd-boot.esp'
+           'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"

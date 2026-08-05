5fcf7fad177fd4615d7284e60a47036af4241cce
iboshka24 <iboshka24@users.noreply.github.com>
2026-08-05 19:25:54 +0500

    fix: use G_APPLICATION_DEFAULT_FLAGS and fix gcc linking order for GTK4

--- installer/main.c
+++ installer/main.c
@@ -444,7 +444,7 @@ static void activate(GtkApplication *gtk_app, gpointer user_data) {
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("org.shobikaos.installer", G_APPLICATION_FLAGS_NONE);
+    GtkApplication *app = gtk_application_new("org.shobikaos.installer", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);

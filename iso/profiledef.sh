6635a17480164cd8693559344bca70894b7a6fb3
iboshka24 <iboshka24@users.noreply.github.com>
2026-08-05 18:13:37 +0500

    fix: set 0440 mode for /etc/sudoers.d/99-liveuser in profiledef.sh

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -18,6 +18,7 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/sudoers.d/99-liveuser"]="0:0:440"
   ["/root"]="0:0:750"
   ["/usr/bin/shobika-live-setup"]="0:0:755"
   ["/usr/bin/shobika-installer"]="0:0:755"
